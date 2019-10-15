29de9c7d08bcb2a394bb04f4fd15137754617b77
Ibrahim Tareq <ibrahim.tareq@news.com.au>
2019-10-15 21:27:52 +1100

    Added rules and deals (WIP)

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ class Ad {
 classicAd = new Ad("Classic", 269.99);
 standoutAd = new Ad("StandOut", 322.99);
 premiumAd = new Ad("Premium", 394.99);
+
+/***** *****/
+
+const rules = [
+    { name: 'SecondBite', shortName: 'SB', deals: [1] },
+    { name: 'AxilCoffeeRoasters', shortName: 'ACR', deals: [2] },
+    { name: 'MYER', shortName: 'MYER', deals: [3,4] }
+];
+
+const deals = [
+    { 
+        id: 1,
+        name: '3 for 2 on Classic Ads', 
+        evaluate : function() {}
+    },
+    { 
+        id: 2,
+        name: 'Standout Ads discount',
+        evaluate : function() {}
+    },
+    { 
+        id: 3, 
+        name: '5 for 4 on Standout Ads',
+        evaluate : function() {}
+    },
+    { 
+        id: 4,
+        name: 'Premium Ads discount',
+        evaluate : function() {}
+    }
+];
